8ad0e8277d7a028696231de8f826ac6ea634e531
Nikhil Raina <nikhilrainathegreat@gmail.com>
2024-11-02 18:05:13 -0700

    Create module5_call.py
    Created the caller and _mod file implementer

---
+++ module5_call.py
@@ -0,0 +1,7 @@
+from module5_mod import NumberFinder
+
+if __name__ == "__main__":
+    number_finder = NumberFinder()
+    number_finder.initiate_input()
+    result = number_finder.find_X()
+    print(result)
